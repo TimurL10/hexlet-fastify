f982c5840fc7a78a2aee75da59f15403d64c00ac
timurl <timurl@contourbi.ru>
2026-06-03 14:30:14 +0300

    hello handler

--- src/index.js
+++ src/index.js
@@ -3,8 +3,45 @@ import fastify from 'fastify'
 const app = fastify()
 const port = 3000
 
+const state = {
+  users: [
+    {
+      id: 1,
+      name: 'user',
+    },
+  ],
+}
+
+app.get('/users', (req, res) => {
+  res.send('GET /users')
+})
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('GET /home')
+})
+
+app.post('/users', (req, res) => {
+  res.send('POST /users')
+})
+
+app.get('/hello',(req,res) => {
+  const {name} = req.query;
+  if (name)
+    res.send(`Hello, ${name}!`)
+  else
+    res.send('Hello, World!')
+  
+})
+
+app.get('/search', (req, res) => {
+  const { id } = req.query
+  const user = state.users.find(user => user.id === parseInt(id)) // Приведение к одному типу и сравнение
+  if (!user) {
+    res.code(404).send({ message: 'User not found' })
+  }
+  else {
+    res.send(user)
+  }
 })
 
 app.listen({ port }, () => {
