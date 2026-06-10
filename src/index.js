19f0d99c7b8669f5a78403bbb21e44bd62dad956
timurl <timurl@contourbi.ru>
2026-06-10 16:05:19 +0300

    html searcj form added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import fastify from 'fastify'
 import view from '@fastify/view'
 import pug from 'pug'
 import cars from './data_pug/cars.js'
+import sanitize from 'sanitize-html'
 
 const app = fastify()
 const port = 3000
@@ -15,34 +16,84 @@ const state = {
       id: 1,
       title: "JS: Массивы",
       description: 'Курс про массивы в JavaScript',
+      level: 'beginner',
+      category: 'backend'
     },
     {
       id: 2,
       title: "JS: Функции",
       description: 'Курс про функции в JavaScript',
+      level: 'beginner',
+      category: 'backend'
     },
+    {
+      id: 3,
+      title: "PL/SQL",
+      description: 'Курс PL/SQL',
+      level: 'advanced',
+      category: 'database'
+    },
+    {
+      id: 4,
+      title: "React JS",
+      description: 'Курс про React JS',
+      level: 'middle',
+      category: 'frontend'
+    },
+    {
+      id: 5,
+      title: "Python",
+      description: 'Курс про Python',
+      level: 'advanced',
+      category: 'backend'
+    },
+    {
+      id: 6,
+      title: "SQL",
+      description: 'Курс про SQL',
+      level: 'beginner',
+      category: 'database'
+    }
   ],
 }
 
 
-
-app.get('/search', (req, res) => {
-  const { id } = req.query
-  const user = state.users.find(user => user.id === parseInt(id)) 
-  if (!user) {
-    res.code(404).send({ message: 'User not found' })
-  }
-  else {
-    res.send(user)
-  }
+//%3Cscript%3Ealert('attack!')%3B%3C%2Fscript%3E
+app.get('/user/:id', (req, res) => {
+  let id  = req.params.id
+   res.type('html');
+   //id = sanitize(id);
+   //res.send(`<html><head></head><body><h1>this id came from you: ${id}</h1></body></html>`);  
+   res.view('/src/views/users.pug', `this id came from you: ${id}`)
 })
+
+
 
 app.get('/courses', (req, res) => {
   const data = {
     courses: state.courses,
     header: 'Курсы по программированию',
   }
-  res.view('/src/views/index.pug', data)
+    const course_name = req.query.term;
+    const level = req.query.level;
+    const category = req.query.category;
+    console.log(course_name);
+    console.log(level);
+    console.log(category);
+
+  if (course_name) {   
+
+    let a =  data.courses.filter(item => item.title.toLowerCase().includes(course_name.toLowerCase()) &&
+                                (item.level == level || item.level == item.level) &&
+                                (item.category == category || item.category == item.category))
+
+    data.courses = a;
+    console.log(JSON.stringify(data));
+    res.view('/src/views/index.pug', data)
+  }
+  else
+    res.view('/src/views/index.pug', data)
+  
 })
 
 app.get('/courses/:id', (req, res) => {
@@ -55,6 +106,8 @@ app.get('/courses/:id', (req, res) => {
     res.view('/src/views/course.pug', { course })
   }  
 })
+
+
 
 
 app.listen({ port }, () => {
