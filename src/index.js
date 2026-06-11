d4d76f902f6c171ba12f7bab230cace330494030
timurl <timurl@contourbi.ru>
2026-06-11 16:28:17 +0300

    users methods aded

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import view from '@fastify/view'
 import pug from 'pug'
 import cars from './data_pug/cars.js'
 import sanitize from 'sanitize-html'
+import formbody from '@fastify/formbody'
+
+
 
 const app = fastify()
 const port = 3000
 
 // Подключаем pug через плагин
 await app.register(view, { engine: { pug } })
+await app.register(formbody);
 
 const state = {
   courses: [
@@ -57,17 +61,71 @@ const state = {
   ],
 }
 
+const state_users = {
+  users: [
+    {
+      id:1,
+      name: "bobdylan",
+      email: "boby1960@yahoo.com",
+      password: '02fjw98rjn3lrkm'
+    },
+    {
+       id:2,
+      name: "Jopo_upa101",
+      email: "Jp-11lopo@gmail.com",
+      password: '543'
+    },
+    {
+       id:3,
+      name: "Johny_Bodrum",
+      email: "JB_Chohen@hotmail.com",
+      password: 'fgh56d454'
+    }
+  ]
+}
 
+/*
 //%3Cscript%3Ealert('attack!')%3B%3C%2Fscript%3E
-app.get('/user/:id', (req, res) => {
+app.get('/users/:id', (req, res) => {
   let id  = req.params.id
    res.type('html');
    //id = sanitize(id);
    //res.send(`<html><head></head><body><h1>this id came from you: ${id}</h1></body></html>`);  
    res.view('/src/views/users.pug', `this id came from you: ${id}`)
 })
+*/
 
 
+app.get('/users/:id', (req, res) => {
+    let {id}  = req.params;
+    let user = state_users.users.find(item => item.id ===  parseInt(id));
+    if (!user) {
+      return reply.code(404).send('User not found')
+    }
+    console.log(user)
+    res.view('/src/views/user.pug', {user})
+})
+
+app.get('/users/new', (req, res) => {
+   //id = sanitize(id);
+   res.view('/src/views/new_users.pug')
+})
+
+app.get('/users', (req, res) => { 
+    res.view('/src/views/users.pug',state_users);
+})
+
+app.post('/users', (req, res) => {  
+    console.log(req.body)
+    let {name, email, password} = req.body;
+    state_users.users.sort((a,b)=> b.id - a.id);
+    let max_id = state_users.users[0].id;
+    let obj = {id: max_id+1,name: name, email:email, password: password}
+    console.log(obj)
+    state_users.users.push(obj);
+    console.log(JSON.stringify(state_users.users))
+    res.view('/src/views/users.pug',state_users)
+})
 
 app.get('/courses', (req, res) => {
   const data = {
